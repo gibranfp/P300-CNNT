61c39c760e66a09399d831df5fc851d213395d8a
amontserratAG <amontserrat@gmail.com>
2019-04-02 20:26:51 -0600

    modificando BN3

--- src/cross_subject_BN3.py
+++ src/cross_subject_BN3.py
@@ -36,21 +36,22 @@ def evaluate_cross_subject_model(data, labels, modelpath):
         X_train, y_train, X_valid, y_valid = data[t], labels[t], data[v], labels[v]
         X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.2, random_state=123)    
         
-        print("Partition {0}: train = {1}, valid = {2}, test = {3}".format(k, X_train.shape, X_valid.shape, X_test.shape))
+        #print("Partition {0}: train = {1}, valid = {2}, test = {3}".format(k, X_train.shape, X_valid.shape, X_test.shape))
+        print("Partition {0}: train = {1}, valid = {2}".format(k, X_train.shape, X_valid.shape))
         sample_weights = class_weight.compute_sample_weight('balanced', y_train)
 
         # one hot encoding
         y_onehot_train = np.zeros((y_train.size, 2))
         y_onehot_valid = np.zeros((y_valid.size, 2))
-        y_onehot_test = np.zeros((y_test.size, 2))
+        #y_onehot_test = np.zeros((y_test.size, 2))
         y_onehot_train[np.arange(y_train.size), y_train.astype(int)] = 1
         y_onehot_valid[np.arange(y_valid.size), y_valid.astype(int)] = 1
-        y_onehot_test[np.arange(y_test.size), y_test.astype(int)] = 1
+        #y_onehot_test[np.arange(y_test.size), y_test.astype(int)] = 1
 
         sc = EEGChannelScaler()
         X_train = sc.fit_transform(X_train)
         X_valid = sc.transform(X_valid)
-        X_test = sc.transform(X_test)
+        #X_test = sc.transform(X_test)
 
         sc = EEGChannelScaler()
         X_train = np.swapaxes(sc.fit_transform(X_train)[:, np.newaxis, :], 2,3)
@@ -70,14 +71,21 @@ def evaluate_cross_subject_model(data, labels, modelpath):
                   callbacks = [es])
 
         model.save(modelpath + '/s' + str(k) + '.h5')
-        proba_test = model.predict(X_test)
-        aucs[k] = roc_auc_score(y_onehot_test, proba_test)
-        accuracies[k] = accuracy_score(y_onehot_test, np.round(proba_test))
+        proba_valid = model.predict(X_valid)
+        aucs[k] = roc_auc_score(y_onehot_valid, proba_valid)
+        accuracies[k] = accuracy_score(y_onehot_valid, np.round(proba_valid))        
+        confusion[k] = confusion_matrix(y_onehot_valid, proba_valid)
+        prec_score[k] = precision_score(y_onehot_valid, proba_valid)
+        f1[k] = f1_score(y_onehot_valid, proba_valid)
+        rec_score[k] = recall_score(y_onehot_valid, proba_valid)
         
-        confusion[k] = confusion_matrix(y_onehot_test, proba_test)
-        prec_score[k] = precision_score(y_onehot_test, proba_test)
-        f1[k] = f1_score(y_onehot_test, proba_test)
-        rec_score[k] = recall_score(y_onehot_test, proba_test)
+        #proba_test = model.predict(X_test)
+        #aucs[k] = roc_auc_score(y_onehot_test, proba_test)
+        #accuracies[k] = accuracy_score(y_onehot_test, np.round(proba_test))        
+        #confusion[k] = confusion_matrix(y_onehot_test, proba_test)
+        #prec_score[k] = precision_score(y_onehot_test, proba_test)
+        #f1[k] = f1_score(y_onehot_test, proba_test)
+        #rec_score[k] = recall_score(y_onehot_test, proba_test)
         
         print('AUC: {0} ACC: {1} Confusion: {2} Precision-Score: {3} F1: {4} Recall-Score: {5}'.format(aucs[k], accuracies[k], confusion[k], prec_score[k], f1[k], rec_score[k]))
     np.savetxt(modelpath + '/aucs.npy', aucs)
