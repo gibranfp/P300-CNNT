191c02da76c9aea4ffdbe045cdc84581e898b7c5
amontserratAG <46611940+amontserratAG@users.noreply.github.com>
2019-06-20 12:28:00 -0500

    Update FCNNmodel.py

--- src/FCNNmodel.py
+++ src/FCNNmodel.py
@@ -11,7 +11,7 @@ def FCNN(Chans = 6, Samples = 206):
     
     input1       = Input(shape = (1,Chans,Samples))
     dense        = Dense(units=2, activation = 'tanh',
-                         kernel_initializer='glorot_uniform', name='densa1')(in$
+                         kernel_initializer='glorot_uniform', name='densa1')(input1)
     flat         = Flatten()(dense)
     prediction   = Dense(units=2, activation = 'sigmoid',name='densa2')(flat)
     
