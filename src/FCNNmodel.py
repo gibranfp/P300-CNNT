0a8513c01c1307258276d8901224e85c5a1e0214
amontserratAG <46611940+amontserratAG@users.noreply.github.com>
2019-06-19 22:31:31 -0500

    Update FCNNmodel.py

--- src/FCNNmodel.py
+++ src/FCNNmodel.py
@@ -2,24 +2,22 @@
 # -*- coding: utf-8
 #
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import os
-from tensorflow.keras.layers import *
 from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input, Flatten
 from tensorflow.keras import backend as K
-
 
 def FCNN(Chans = 6, Samples = 206):
     
-    input1       = Input(shape = (Samples, Chans))
-    block1       = BatchNormalization(axis = 1)(input1)
-    ##################################################################
-    flatten      = Flatten(name = 'flatten')(block1)
-    dense        = Dense(2, activation = 'tanh',
-		   	 kernel_initializer='glorot_uniform')(flatten)
-    prediction   = Dense(1, activation = 'sigmoid')(dense)
+    input1       = Input(shape = (1,Chans,Samples))
+    dense        = Dense(units=2, activation = 'tanh',
+                         kernel_initializer='glorot_uniform', name='densa1')(in$
+    flat         = Flatten()(dense)
+    prediction   = Dense(units=2, activation = 'sigmoid',name='densa2')(flat)
     
+
     return Model(inputs=input1, outputs=prediction, name='FCNN')
+
+
+
+
