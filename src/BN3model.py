e013db37352533ae7f611750efb617b188c95408
Gibran Fuentes-Pineda <gibranfp@unam.mx>
2019-07-02 00:07:38 -0500

    Bugfixes

--- src/BN3model.py
+++ src/BN3model.py
@@ -6,7 +6,8 @@
 # 2019
 #
 '''
- Keras implementation of Liu's BN^3 architecture (https://www.sciencedirect.com/science/article/pii/S0925231217314601). Requires 'image_data_format' to be set as 'channels_first' in keras.json config file. 
+Keras implementation of Liu's BN^3 architecture (https://www.sciencedirect.com/science/article/pii/S0925231217314601).
+Requires 'image_data_format' to be set as 'channels_first' in keras.json config file. 
 '''
 
 from __future__ import absolute_import
@@ -19,24 +20,21 @@ from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
 
 
-def BN3(Chans = 6, Samples = 206):
-    input1       = Input(shape = (Samples, Chans))
-    block1       = BatchNormalization()(input1)
-    block1       = Conv1D(16, 1, stride = 1, padding = 'same', use_bias = True,
-                          data_format = 'channels_last')(block1)
-    block1       = Activation('relu')(block1)
-    block2       = Conv1D(10, (20,1), stride=20, padding = 'same', use_bias = True,
-                          data_format = 'channels_last')(block1)
-    block2       = Activation('relu')(block2)    
-    block2       = BatchNormalization()(block2)
+def BN3(Samples = 206, Chans = 6):
+    eeg_input   = Input(shape = (Samples, Chans))
+    bn_input    = BatchNormalization()(eeg_input)
+    block1       = Conv1D(16, 1, strides = 1, padding = 'valid', use_bias = True,
+                          data_format = 'channels_last', bias_initializer = 'glorot_uniform', kernel_initializer = 'glorot_uniform')(bn_input)
+    block2       = Conv1D(16, 20, strides = 20, padding = 'same', use_bias = True,
+                          data_format = 'channels_last', bias_initializer = 'glorot_uniform', kernel_initializer = 'glorot_uniform')(block1)
+    block2      = BatchNormalization()(block2)
+    block2       = Activation('relu')(block2)
     
     flatten      = Flatten(name = 'flatten')(block2)
-    dense        = Dense(128, activation = 'tanh')(flatten)
-    block1       = Dropout(0.8)(dense)
-    dense        = Dense(128, activation = 'tanh')(block1)
-    block1       = Dropout(0.8)(dense)
-    prediction   = Dense(1, activation = 'sigmoid')(block1)
+    dense1       = Dense(128, activation = 'tanh', bias_initializer = 'glorot_uniform', kernel_initializer = 'glorot_uniform')(flatten)
+    dense1       = Dropout(0.8)(dense1)
+    dense2       = Dense(128, activation = 'tanh', bias_initializer = 'glorot_uniform', kernel_initializer = 'glorot_uniform')(dense1)
+    dense2       = Dropout(0.8)(dense2)
+    prediction   = Dense(1, activation = 'sigmoid', bias_initializer = 'glorot_uniform', kernel_initializer = 'glorot_uniform')(dense2)
     
-    return Model(inputs = input1, outputs = prediction, name = 'BN3')
-
-
+    return Model(inputs = eeg_input, outputs = prediction, name = 'BN3')
