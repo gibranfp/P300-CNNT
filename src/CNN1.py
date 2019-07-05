d460124f007d705928ec09bfd75e3f9f3d5e3c63
Gibran Fuentes-Pineda <gibranfp@unam.mx>
2019-07-05 02:25:56 -0500

    Fixes CNN1 architecture

--- src/CNN1.py
+++ src/CNN1.py
@@ -13,19 +13,41 @@ import os
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
+from keras.utils.generic_utils import get_custom_objects
 
-def CNN1(Chans = 6,Samples=206):
-    input1       = Input(shape = (Chans, Samples))
-    ##################################################################
-    block1       = Conv2D(?, ?, padding = 'same',
-                                   input_shape = (Chans, Samples),
-                                   use_bias = False)(input1)
-    block1       = Activation('tanh')(block1)
-    block1       = Conv2D(?, (1, 128), padding = 'same',
-                                   input_shape = (Chans, Samples),
-                                   use_bias = False)(input1)
-    block1       = Activation('tanh')(block1)
+def scaled_tanh(z):
+    '''
+    Scaled hyperbolic tangent activation function, as proposed
+    by Lecun 1989:
+    http://yann.lecun.com/exdb/publis/pdf/lecun-89.pdf
+
+    See also Lecun et al. 1998:
+    http://yann.lecun.com/exdb/publis/pdf/lecun-98b.pdf 
+    '''
+    return 1.7159 * K.tanh((2.0 / 3.0) * z)
+
+get_custom_objects().update({'scaled_tanh': Activation(scaled_tanh)})
+
+def CNN1(Chans = 6, Samples = 206):
+    eeg_input    = Input(shape = (Samples, Chans))
+    
+    block1       = Conv1D(10, 1, padding = 'same',
+                          data_format = 'channels_last',
+                          bias_initializer = 'glorot_uniform',
+                          kernel_initializer = 'glorot_uniform',
+                          use_bias = True)(eeg_input)
+    block1       = Activation('scaled_tanh')(block1)
+
+    block1       = Conv1D(50, 13, padding = 'same',
+                          data_format = 'channels_last',
+                          bias_initializer = 'glorot_uniform',
+                          kernel_initializer = 'glorot_uniform',
+                          use_bias = True)(block1)
+    block1       = Activation('scaled_tanh')(block1)
+    
     flatten      = Flatten(name = 'flatten')(block1)
-    dense        = Dense(100,input_dim=Chans, activation = 'sigmoid')(flatten)
-    prediction   = Dense(2, activation = 'sigmoid')(dense)
-  return Model(inputs=input1, outputs=prediction, name='CNN1')
+    dense        = Dense(100, activation = 'sigmoid')(flatten)
+    dense        = Dense(2)(dense)
+    softmax      = Activation('softmax', name = 'softmax')(dense)
+    
+    return Model(inputs = eeg_input, outputs = softmax, name = 'CNN1')
---
+++ src/single_trial_cnn1.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8
+#
+# Gibran Fuentes-Pineda <gibranfp@unam.mx>
+# IIMAS, UNAM
+# 2018
+#
+"""
+Script to evaluate the CNN1 architecture (Lawhern et al., 2018) for single-trial subject-dependent P300 detection 
+"""
+import argparse
+import sys
+import numpy as np
+from CNN1 import CNN1
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import *
+from utils import *
+import pickle
+
+def evaluate_subject_models(data, labels, modelpath):
+    """
+    Trains and evaluates CNN1 for each subject in the P300 Speller database
+    using repeated stratified K-fold cross validation.
+    """
+    n_sub = data.shape[0]
+    n_ex_sub = data.shape[1]
+    n_samples = data.shape[2]
+    n_channels = data.shape[3]
+    for i in range(data.shape[0]):
+        aucs = np.zeros(10 * 10)
+        accuracies = np.zeros(10 * 10)
+        precisions = np.zeros(10 * 10)
+        recalls = np.zeros(10 * 10)
+        aps = np.zeros(10 * 10)
+        f1scores = np.zeros(10 * 10)
+
+        print("Training for subject {0}: ".format(i))
+        cv = RepeatedStratifiedKFold(n_splits = 5, n_repeats = 10, random_state = 123)
+        for k, (t, v) in enumerate(cv.split(data[i], labels[i])):
+            X_train, y_train, X_test, y_test = data[i, t, :, :], labels[i, t], data[i, v, :, :], labels[i, v]
+            X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size = 0.2, shuffle = True, random_state = 456)
+            print('Partition {0}: X_train = {1}, X_valid = {2}, X_test = {3}'.format(k, X_train.shape, X_valid.shape, X_test.shape))
+
+            # channel-wise feature standarization
+            sc = EEGChannelScaler()
+            X_train = sc.fit_transform(X_train)
+            X_valid = sc.transform(X_valid)
+            X_test = sc.transform(X_test)
+
+            model = CNN1(2, Chans = 6, Samples = 206)
+            print(model.summary())
+            model.compile(optimizer = 'adam', loss = 'categorical_crossentropy')
+            
+            # Early stopping setting also follows EEGNet (Lawhern et al., 2018)
+            es = EarlyStopping(monitor = 'val_loss', mode = 'min', patience = 50, restore_best_weights = True)
+            history = model.fit(X_train,
+                                to_categorical(y_train),
+                                batch_size = 256,
+                                epochs = 200,
+                                validation_data = (X_valid, to_categorical(y_valid)),
+                                callbacks = [es])
+
+            model.save(modelpath + '/s' + str(i) + 'p' + str(k) + '.h5')
+            with open(modelpath + '/s' + str(k) + '.history', 'wb') as fh:
+                pickle.dump(history.history, fh)
+
+            proba_test = model.predict(X_test)
+            aucs[k] = roc_auc_score(y_test, proba_test[:, 1])
+            accuracies[k] = accuracy_score(y_test, proba_test[:, 1].round())
+            precisions[k] = precision_score(y_test, proba_test[:, 1].round())
+            recalls[k] = recall_score(y_test, proba_test[:, 1].round())
+            aps[k] = average_precision_score(y_test, proba_test[:, 1])
+            f1scores[k] = f1_score(y_test, proba_test[:, 1].round())
+            print('AUC: {0} ACC: {1} PRE: {2} REC: {3} AP: {4} F1: {5}'.format(aucs[k],
+                                                                               accuracies[k],
+                                                                               precisions[k],
+                                                                               recalls[k],
+                                                                               aps[k],
+                                                                               f1scores[k]))
+        np.savetxt(modelpath + '/s' + str(i) + '_aucs.npy', aucs)
+        np.savetxt(modelpath + '/s' + str(i) + '_accuracies.npy', accuracies)
+        np.savetxt(modelpath + '/s' + str(i) + '_precisions.npy', precisions)
+        np.savetxt(modelpath  + '/s' + str(i) + '_recalls.npy', recalls)
+        np.savetxt(modelpath  + '/s' + str(i) + '_aps.npy', aps)
+        np.savetxt(modelpath  + '/s' + str(i) + '_f1scores.npy', f1scores)
+            
+def main():
+    """
+    Main function
+    """
+    try:
+        parser = argparse.ArgumentParser()
+        parser = argparse.ArgumentParser(
+            description="Evaluates single-trial subject-dependent P300 detection using cross-validation")
+        parser.add_argument("datapath", type=str,
+                            help="Path for the data of the P300 Speller Database (NumPy file)")
+        parser.add_argument("labelspath", type=str,
+                            help="Path for the labels of the P300 Speller Database (NumPy file)")
+        parser.add_argument("modelpath", type=str,
+                            help="Path of the directory where the models are to be saved")
+        args = parser.parse_args()
+        
+        np.random.seed(0)
+        data, labels = load_db(args.datapath, args.labelspath)
+        evaluate_subject_models(data, labels, args.modelpath)
+        
+    except SystemExit:
+        print('for help use --help')
+        sys.exit(2)
+        
+if __name__ == "__main__":
+    main()
