0bffc502eb23656dc479248dffd08329df6c207a
Gibran Fuentes-Pineda <gibranfp@unam.mx>
2019-07-08 01:22:53 -0500

    Changes to subject by subject evaluation

--- src/single_trial_FCNN.py
+++ src/single_trial_FCNN.py
@@ -15,9 +15,8 @@ from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.model_selection import *
 from FCNNmodel import FCNN
 from utils import *
-import pickle
 
-def evaluate_subject_models(data, labels, modelpath):
+def evaluate_subject_models(data, labels, modelpath, subject):
     """
     Trains and evaluates the FCNN architecture for each subject in the P300 Speller database
     using leave one group out cross validation.
@@ -26,39 +25,38 @@ def evaluate_subject_models(data, labels, modelpath):
     n_ex_sub = data.shape[1]
     n_samples = data.shape[2]
     n_channels = data.shape[3]
-    for i in range(data.shape[0]):
-        aucs = np.zeros(5 * 10)
-        print("Training for subject {0}: ".format(i))
-        cv = RepeatedStratifiedKFold(n_splits = 5, n_repeats = 10, random_state = 123)
-        for k, (t, v) in enumerate(cv.split(data[i], labels[i])):
-            X_train, y_train, X_test, y_test = data[i, t, :, :], labels[i, t], data[i, v, :, :], labels[i, v]
-            X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size = 0.2, shuffle = True, random_state = 456)
-            print('Partition {0}: X_train = {1}, X_valid = {2}, X_test = {3}'.format(k, X_train.shape, X_valid.shape, X_test.shape))
+    aucs = np.zeros(5 * 10)
+    print("Training for subject {0}: ".format(subject))
+    cv = RepeatedStratifiedKFold(n_splits = 5, n_repeats = 10, random_state = 123)
+    for k, (t, v) in enumerate(cv.split(data[subject], labels[subject])):
+        X_train, y_train, X_test, y_test = data[subject, t, :, :], labels[subject, t], data[subject, v, :, :], labels[subject, v]
+        X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size = 0.2, shuffle = True, random_state = 456)
+        print('Partition {0}: X_train = {1}, X_valid = {2}, X_test = {3}'.format(k, X_train.shape, X_valid.shape, X_test.shape))
 
-            # channel-wise feature standarization
-            sc = EEGChannelScaler()
-            X_train = sc.fit_transform(X_train).reshape(X_train.shape[0], X_train.shape[1] * X_train.shape[2])
-            X_valid = sc.transform(X_valid).reshape(X_valid.shape[0], X_valid.shape[1] * X_valid.shape[2])
-            X_test = sc.transform(X_test).reshape(X_test.shape[0], X_test.shape[1] * X_test.shape[2])
+        # channel-wise feature standarization
+        sc = EEGChannelScaler()
+        X_train = sc.fit_transform(X_train).reshape(X_train.shape[0], X_train.shape[1] * X_train.shape[2])
+        X_valid = sc.transform(X_valid).reshape(X_valid.shape[0], X_valid.shape[1] * X_valid.shape[2])
+        X_test = sc.transform(X_test).reshape(X_test.shape[0], X_test.shape[1] * X_test.shape[2])
 
-            model = FCNN()
-            print(model.summary())
-            model.compile(optimizer = 'adam', loss = 'binary_crossentropy')
+        model = FCNN()
+        print(model.summary())
+        model.compile(optimizer = 'adam', loss = 'binary_crossentropy')
 
-            # Early stopping setting also follows EEGNet (Lawhern et al., 2018)
-            es = EarlyStopping(monitor = 'val_loss', mode = 'min', patience = 50, restore_best_weights = True)
-            history = model.fit(X_train,
-                                y_train,
-                                batch_size = 256,
-                                epochs = 200,
-                                validation_data = (X_valid, y_valid),
-                                callbacks = [es])
+        # Early stopping setting also follows EEGNet (Lawhern et al., 2018)
+        es = EarlyStopping(monitor = 'val_loss', mode = 'min', patience = 50, restore_best_weights = True)
+        history = model.fit(X_train,
+                            y_train,
+                            batch_size = 256,
+                            epochs = 200,
+                            validation_data = (X_valid, y_valid),
+                            callbacks = [es])
 
-            proba_test = model.predict(X_test)
-            aucs[k] = roc_auc_score(y_test, proba_test)
-            print('AUC: {0}'.format(aucs[k]))
+        proba_test = model.predict(X_test)
+        aucs[k] = roc_auc_score(y_test, proba_test)
+        print('S{0}, P{1} -- AUC: {2}'.format(subject, k, aucs[k]))
 
-        np.savetxt(modelpath + '/s' + str(i) + '_aucs.npy', aucs)
+    np.savetxt(modelpath + '/s' + str(subject) + '_aucs.npy', aucs)
             
 def main():
     """
@@ -74,10 +72,14 @@ def main():
                             help="Path for the labels of the P300 Speller Database (NumPy file)")
         parser.add_argument("modelpath", type=str,
                             help="Path of the directory where the models are to be saved")
+        parser.add_argument("--subject", type=int,
+                            help="Subject to evaluate")
         args = parser.parse_args()
 
+        np.random.seed(1)
+
         data, labels = load_db(args.datapath, args.labelspath)
-        evaluate_subject_models(data, labels, args.modelpath)
+        evaluate_subject_models(data, labels, args.modelpath, args.subject)
         
     except SystemExit:
         print('for help use --help')
