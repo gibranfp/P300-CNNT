9e8d5baab063762026f94bbbc5635f12e7344360
Gibran Fuentes-Pineda <gibranfp@unam.mx>
2021-02-15 15:32:23 -0600

    Removes old scripts

--- scripts/run_save_cross.sh
+++
@@ -1,5 +0,0 @@
-for f in 1 2 4 8;
-do
-    echo "$f filters"
-    python3 src/save_cross_SepConv1D.py $1 $2 $3/$f/ --n_filters $f
-done
--- scripts/run_subject_specific.sh
+++
@@ -1,4 +0,0 @@
-for i in `seq 0 $1`;
-do
-    python3 $2 $3 $4 $5 --subject $i
-done
